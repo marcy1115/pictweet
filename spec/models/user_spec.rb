e4bfb33dafd9348d40c3e19519fe6229373aa06f
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-07-19 16:22:49 +0900

    tweet RSpec記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,12 +44,19 @@ describe User, type: :model do
         expect( another_user.errors.full_messages ).to include( "Email has already been taken" )
       end
       it "passwordが空では登録できない" do
-        
+        @user.password = ''
+        @user.valid?
+        expect( @user.errors.full_messages ).to include( "Password confirmation doesn't match Password" )
       end
       it "passwordが5文字以下であれば登録できない" do
+        @user.password ='00000'
+        @user.valid?
+        expect( @user.errors.full_messages ).to include( "Password is too short (minimum is 6 characters)" )
       end
       it "passwordが存在してもpassword_confirmationが空では登録できない" do
-        
+        @user.password_confirmation = ''
+        @user.valid?
+        expect( @user.errors.full_messages ).to include( "Password confirmation doesn't match Password" )
       end
     end
   end
